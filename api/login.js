7e9d6374e936a2e08ca0f270b09fbca9073d37c4
pamanbobb <ngelurevolution5@gmail.com>
2021-09-30 04:46:01 +0700

    second commit

--- api/login.js
+++ api/login.js
@@ -3,22 +3,23 @@ const router = express.Router();
 const Insta = require('@androz2091/insta.js');
 const client = new Insta.Client();
 
-router.get('/', async function(req, res){
+router.get('/', (async function(req, res){
     const {username = 'username'} = req.query;
     const {sandi = 'sandi'} = req.query;
     try {
         res.setHeader('Content-Type', 'application/json');
         res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
         client.login(`${username}`,`${sandi}`);
-            client.on('connected', () => {
-            client.fetchUser(`${username}`)
-                .then((user) => {
-                    res.status(200).send(user);
+            await client.on('connected', () => {
+                client.fetchUser(`${username}`)
+                    .then((user) => {
+                        res.status(200).send(user);
+                });
             });
-        });
-    }catch(e) {
-        res.status(e.statusCode || 500).json({'status': e.message});
+    }catch(err) {
+        res.status(err.statusCode || 500).json({'status': err.message});
+        throw err
     }
-});
+})().catch( e => { console.error(e) }));
 
 module.exports = router;
